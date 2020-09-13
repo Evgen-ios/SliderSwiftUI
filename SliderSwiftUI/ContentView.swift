c8e25610b3f018979e7ab1af3174f8c52ac46628
Evgen-ios <jack38irk@icloud.com>
2020-09-13 20:58:04 +0800

    Initial Commit

---
+++ SliderSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SliderSwiftUI
+//
+//  Created by Евгений on 13.09.2020.
+//  Copyright © 2020 Goncharov Evgenii. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
